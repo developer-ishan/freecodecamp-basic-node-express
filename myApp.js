6faca6e08399e6af90993003e03aa28a3b8381ff
developer-ishan <62565307+developer-ishan@users.noreply.github.com>
2021-09-22 18:37:48 +0000

    Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -1,19 +1,52 @@
 var express = require('express');
 var app = express();
+var logger = require("./logger");
 console.log("Hello World");
 
 
 // app.get("/", (req, res) => {
 //   res.send("Hello Express")
 // })
-
+app.use(logger)
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 })
 
+app.get("/json", (req, res) => {
+  let message = "Hello json";
+  if(process.env["MESSAGE_STYLE"] === "uppercase")
+    message = message.toUpperCase();
+  res.json({"message": message});
+})
 
+app.get("/now", 
+  (req,res,next) => {
+    req.time = new Date().toString();
+    next()
+  }, 
+  (req, res) => res.json({time: req.time})
+)
+app.get("/:word/echo", 
+  (req, res) => {
+    res.json({echo: req.params.word});
+  }
+)
 
+// app.get("name",
+//   (req, res) => {
+//     res.json({
+//       name: `${req.query.first} ${req.query.last}`
+//     })
+//   }
+// )
 
+app.route("/name")
+  .get((req, res) => {
+    res.json({
+      name: `${req.query.first} ${req.query.last}`
+    })
+  })
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
